5810cc189fbd63982f96d54674e71bd09db6dedb
Emmanuel Raviart <emmanuel@raviart.com>
2016-11-11 22:02:54 +0100

    Cleanup autocomplete code.

--- src/Autocomplete.elm
+++ src/Autocomplete.elm
@@ -1,40 +1,31 @@
 module Autocomplete
     exposing
-        ( view
-        , update
-        , subscription
-        , viewConfig
-        , updateConfig
-        , State
-        , empty
-        , reset
-        , resetToFirstItem
-        , resetToLastItem
+        ( empty
+        , HtmlDetails
         , KeySelected
         , MouseSelected
         , Msg
-        , ViewConfig
+        , reset
+        , resetToFirstItem
+        , resetToLastItem
+        , State
+        , subscription
+        , update
         , UpdateConfig
-        , HtmlDetails
+        , view
+        , ViewConfig
         )
 
 import Char exposing (KeyCode)
 import Html exposing (Html, Attribute)
 import Html.App
 import Html.Attributes
-import Html.Keyed
 import Html.Events
+import Html.Keyed
 import Keyboard
 
 
-
 -- MODEL
-
-
-type alias State =
-    { key : Maybe String
-    , mouse : Maybe String
-    }
 
 
 type alias KeySelected =
@@ -43,6 +34,12 @@ type alias KeySelected =
 
 type alias MouseSelected =
     Bool
+
+
+type alias State =
+    { key : Maybe String
+    , mouse : Maybe String
+    }
 
 
 empty : State
@@ -56,11 +53,6 @@ reset { separateSelections } { key, mouse } =
         { key = Nothing, mouse = mouse }
     else
         empty
-
-
-resetToFirstItem : UpdateConfig msg data -> List data -> Int -> State -> State
-resetToFirstItem config data howManyToShow state =
-    resetToFirst config (List.take howManyToShow data) state
 
 
 resetToFirst : UpdateConfig msg data -> List data -> State -> State
@@ -85,6 +77,11 @@ resetToFirst config data state =
                         |> setFirstItem datum
 
 
+resetToFirstItem : UpdateConfig msg data -> List data -> Int -> State -> State
+resetToFirstItem config data howManyToShow state =
+    resetToFirst config (List.take howManyToShow data) state
+
+
 resetToLastItem : UpdateConfig msg data -> List data -> Int -> State -> State
 resetToLastItem config data howManyToShow state =
     let
@@ -98,13 +95,6 @@ resetToLastItem config data howManyToShow state =
 -- UPDATE
 
 
-{-| Add this to your `program`s subscriptions to animate the spinner.
--}
-subscription : Sub Msg
-subscription =
-    Keyboard.downs KeyDown
-
-
 type Msg
     = Focus String
     | KeyDown KeyCode
@@ -115,49 +105,65 @@ type Msg
 
 
 type alias UpdateConfig msg data =
-    { onKeyDown : KeyCode -> Maybe String -> Maybe msg
-    , onTooLow : Maybe msg
-    , onTooHigh : Maybe msg
-    , onFocus : String -> Maybe msg
-    , onMouseEnter : String -> Maybe msg
-    , onMouseLeave : String -> Maybe msg
-    , onMouseClick : String -> Maybe msg
-    , toId : data -> String
-    , separateSelections : Bool
-    }
-
-
-updateConfig :
-    { toId : data -> String
+    { onFocus : String -> Maybe msg
     , onKeyDown : KeyCode -> Maybe String -> Maybe msg
-    , onTooLow : Maybe msg
-    , onTooHigh : Maybe msg
-    , onFocus : String -> Maybe msg
+    , onMouseClick : String -> Maybe msg
     , onMouseEnter : String -> Maybe msg
     , onMouseLeave : String -> Maybe msg
-    , onMouseClick : String -> Maybe msg
+    , onTooHigh : Maybe msg
+    , onTooLow : Maybe msg
     , separateSelections : Bool
+    , toId : data -> String
     }
-    -> UpdateConfig msg data
-updateConfig { toId, onKeyDown, onTooLow, onTooHigh, onFocus, onMouseEnter, onMouseLeave, onMouseClick, separateSelections } =
-    { toId = toId
-    , onKeyDown = onKeyDown
-    , onTooLow = onTooLow
-    , onTooHigh = onTooHigh
-    , onFocus = onFocus
-    , onMouseEnter = onMouseEnter
-    , onMouseLeave = onMouseLeave
-    , onMouseClick = onMouseClick
-    , separateSelections = separateSelections
-    }
+
+
+getNextItemId : List String -> String -> String
+getNextItemId ids selectedId =
+    Maybe.withDefault selectedId <| List.foldl (getPrevious selectedId) Nothing ids
+
+
+getPrevious : String -> String -> Maybe String -> Maybe String
+getPrevious id selectedId resultId =
+    if selectedId == id then
+        Just id
+    else if (Maybe.withDefault "" resultId) == id then
+        Just selectedId
+    else
+        resultId
+
+
+getPreviousItemId : List String -> String -> String
+getPreviousItemId ids selectedId =
+    Maybe.withDefault selectedId <| List.foldr (getPrevious selectedId) Nothing ids
+
+
+navigateWithKey : Int -> List String -> Maybe String -> Maybe String
+navigateWithKey code ids maybeId =
+    case code of
+        38 ->
+            Maybe.map (getPreviousItemId ids) maybeId
+
+        40 ->
+            Maybe.map (getNextItemId ids) maybeId
+
+        _ ->
+            maybeId
+
+
+resetMouseStateWithId : Bool -> String -> State -> State
+resetMouseStateWithId separateSelections id state =
+    if separateSelections then
+        { key = state.key, mouse = Just id }
+    else
+        { key = Just id, mouse = Just id }
 
 
 update : UpdateConfig msg data -> Msg -> Int -> State -> List data -> ( State, Maybe msg )
 update config msg howManyToShow state data =
     case msg of
         Focus id ->
-            (state, config.onFocus id)
-    
+            ( state, config.onFocus id )
+
         KeyDown keyCode ->
             let
                 boundedList =
@@ -184,6 +190,11 @@ update config msg howManyToShow state data =
                     , config.onKeyDown keyCode newKey
                     )
 
+        MouseClick id ->
+            ( resetMouseStateWithId config.separateSelections id state
+            , config.onMouseClick id
+            )
+
         MouseEnter id ->
             ( resetMouseStateWithId config.separateSelections id state
             , config.onMouseEnter id
@@ -194,57 +205,27 @@ update config msg howManyToShow state data =
             , config.onMouseLeave id
             )
 
-        MouseClick id ->
-            ( resetMouseStateWithId config.separateSelections id state
-            , config.onMouseClick id
-            )
-
         NoOp ->
             ( state, Nothing )
 
 
-resetMouseStateWithId : Bool -> String -> State -> State
-resetMouseStateWithId separateSelections id state =
-    if separateSelections then
-        { key = state.key, mouse = Just id }
-    else
-        { key = Just id, mouse = Just id }
+
+-- VIEW
 
 
-getPreviousItemId : List String -> String -> String
-getPreviousItemId ids selectedId =
-    Maybe.withDefault selectedId <| List.foldr (getPrevious selectedId) Nothing ids
+type alias HtmlDetails msg =
+    { attributes : List (Attribute msg)
+    , children : List (Html msg)
+    }
 
 
-getPrevious : String -> String -> Maybe String -> Maybe String
-getPrevious id selectedId resultId =
-    if selectedId == id then
-        Just id
-    else if (Maybe.withDefault "" resultId) == id then
-        Just selectedId
-    else
-        resultId
+type alias ViewConfig data =
+    { toId : data -> String
+    , viewItemContent : data -> List (Html Never)
+    }
 
 
-getNextItemId : List String -> String -> String
-getNextItemId ids selectedId =
-    Maybe.withDefault selectedId <| List.foldl (getPrevious selectedId) Nothing ids
-
-
-navigateWithKey : Int -> List String -> Maybe String -> Maybe String
-navigateWithKey code ids maybeId =
-    case code of
-        38 ->
-            Maybe.map (getPreviousItemId ids) maybeId
-
-        40 ->
-            Maybe.map (getNextItemId ids) maybeId
-
-        _ ->
-            maybeId
-
-
-keyedDiv : List (Attribute Msg) -> List (String, Html Msg) -> Html Msg
+keyedDiv : List (Attribute Msg) -> List ( String, Html Msg ) -> Html Msg
 keyedDiv attributes children =
     (Html.Keyed.node "div") attributes children
 
@@ -281,7 +262,7 @@ viewItem { toId, viewItemContent } { key, mouse } data =
             , Html.Events.onMouseLeave (MouseLeave id)
             , Html.Events.onClick (MouseClick id)
             ]
-            (List.map (Html.App.map (\html -> NoOp)) (viewItemContent  data))
+            (List.map (Html.App.map (\html -> NoOp)) (viewItemContent data))
 
 
 viewList : ViewConfig data -> Int -> State -> List data -> Html Msg
@@ -296,28 +277,12 @@ viewList config howManyToShow state data =
             )
 
 
-type alias HtmlDetails msg =
-    { attributes : List (Attribute msg)
-    , children : List (Html msg)
-    }
+
+-- SUBSCRIPTIONS
 
 
-type alias ViewConfig data =
-    { toId : data -> String
-    -- , ul : List (Attribute Never)
-    , viewItemContent : data -> List (Html Never)
-    }
-
-
-viewConfig :
-    { toId : data -> String
-    -- , ul : List (Attribute Never)
-    , viewItemContent : data -> List (Html Never)
-    }
-    -> ViewConfig data
--- viewConfig { toId, ul, viewItemContent } =
-viewConfig { toId, viewItemContent } =
-    { toId = toId
-    -- , ul = ul
-    , viewItemContent = viewItemContent
-    }
+{-| Add this to your `program`s subscriptions to animate the spinner.
+-}
+subscription : Sub Msg
+subscription =
+    Keyboard.downs KeyDown
