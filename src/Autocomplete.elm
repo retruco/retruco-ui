3c270bc65cceb5a8a164c15cd5e3e98143a352a4
Emmanuel Raviart <emmanuel@raviart.com>
2016-11-11 21:38:41 +0100

    Simplify autocompleter and remove native trick.

--- src/Autocomplete.elm
+++ src/Autocomplete.elm
@@ -16,362 +16,308 @@ module Autocomplete
         , ViewConfig
         , UpdateConfig
         , HtmlDetails
-        , viewWithSections
-        , sectionConfig
-        , viewWithSectionsConfig
-        , SectionNode
-        , SectionConfig
-        , ViewWithSectionsConfig
         )
 
-{-|
- This library helps you create an autocomplete menu.
- Your data is stored separately; keep it in whatever shape makes the most sense for your application.
- An autocomplete has a lot of uses: form input, mentions, search, etc.
-
- I have (hopefully!) given the users of this library a large amount of customizability.
-
- I recommend looking at the [examples](https://github.com/thebritican/elm-autocomplete/tree/master/examples) before diving into the API or source code.
-
-# View
-@docs view
-
-# Update
-@docs update, subscription
-
-# Configuration
-@docs viewConfig, updateConfig
-
-# State
-@docs State, empty, reset, resetToFirstItem, resetToLastItem, KeySelected, MouseSelected
-
-# Definitions
-@docs Msg, ViewConfig, UpdateConfig, HtmlDetails
-
-# Sections
-Sections require a separate view and configuration since another type of data must be
-provided: sections.
-
-**Note:** Section data can have any shape: your static configuration will
-just tell the autocomplete how to grab an ID for a section and its related data.
-
-# View
-@docs viewWithSections
-
-# Configuration
-@docs sectionConfig, viewWithSectionsConfig
-
-# Definitions
-@docs SectionNode, SectionConfig, ViewWithSectionsConfig
-
--}
-
-import Autocomplete.Autocomplete as Internal
-import Html exposing (..)
-import Html.App as Html
 import Char exposing (KeyCode)
+import Html exposing (Html, Attribute)
+import Html.App
+import Html.Attributes
+import Html.Keyed
+import Html.Events
+import Keyboard
 
 
-{-| Tracks keyboard and mouse selection within the menu.
--}
-type State
-    = State Internal.State
+
+-- MODEL
 
 
-{-| True if the element has been selected via keyboard navigation.
--}
+type alias State =
+    { key : Maybe String
+    , mouse : Maybe String
+    }
+
+
 type alias KeySelected =
     Bool
 
 
-{-| True if the element has been selected via mouse hover.
--}
 type alias MouseSelected =
     Bool
 
 
-{-| A State with nothing selected.
--}
 empty : State
 empty =
-    State Internal.empty
+    { key = Nothing, mouse = Nothing }
 
 
-{-| Reset the keyboard navigation but leave the mouse state alone.
-Convenient when the two selections are represented separately.
--}
 reset : UpdateConfig msg data -> State -> State
-reset (UpdateConfig config) (State state) =
-    State <| Internal.reset config state
+reset { separateSelections } { key, mouse } =
+    if separateSelections then
+        { key = Nothing, mouse = mouse }
+    else
+        empty
 
 
-{-| Like `reset` but defaults to a keyboard selection of the first item.
--}
 resetToFirstItem : UpdateConfig msg data -> List data -> Int -> State -> State
-resetToFirstItem (UpdateConfig config) data howManyToShow (State state) =
-    State <| Internal.resetToFirstItem config data howManyToShow state
+resetToFirstItem config data howManyToShow state =
+    resetToFirst config (List.take howManyToShow data) state
 
 
-{-| Like `reset` but defaults to a keyboard selection of the last item.
--}
+resetToFirst : UpdateConfig msg data -> List data -> State -> State
+resetToFirst config data state =
+    let
+        { toId, separateSelections } =
+            config
+
+        setFirstItem datum newState =
+            { newState | key = Just <| toId <| Debug.log "resetToFirst datum" datum }
+    in
+        case List.head data of
+            Nothing ->
+                empty
+
+            Just datum ->
+                if separateSelections then
+                    reset config state
+                        |> setFirstItem datum
+                else
+                    empty
+                        |> setFirstItem datum
+
+
 resetToLastItem : UpdateConfig msg data -> List data -> Int -> State -> State
-resetToLastItem (UpdateConfig config) data howManyToShow (State state) =
-    State <| Internal.resetToLastItem config data howManyToShow state
+resetToLastItem config data howManyToShow state =
+    let
+        reversedData =
+            List.reverse <| List.take howManyToShow data
+    in
+        resetToFirst config reversedData state
 
 
 
 -- UPDATE
 
 
-{-| A message type for the autocomplete to update.
+{-| Add this to your `program`s subscriptions to animate the spinner.
 -}
+subscription : Sub Msg
+subscription =
+    Keyboard.downs KeyDown
+
+
 type Msg
-    = Msg Internal.Msg
+    = Focus String
+    | KeyDown KeyCode
+    | MouseEnter String
+    | MouseLeave String
+    | MouseClick String
+    | NoOp
 
 
-{-| Configuration for updates
--}
-type UpdateConfig msg data
-    = UpdateConfig (Internal.UpdateConfig msg data)
+type alias UpdateConfig msg data =
+    { onKeyDown : KeyCode -> Maybe String -> Maybe msg
+    , onTooLow : Maybe msg
+    , onTooHigh : Maybe msg
+    , onFocus : String -> Maybe msg
+    , onMouseEnter : String -> Maybe msg
+    , onMouseLeave : String -> Maybe msg
+    , onMouseClick : String -> Maybe msg
+    , toId : data -> String
+    , separateSelections : Bool
+    }
 
 
-{-| Use this function to update the autocomplete's `State`.
-Provide the same data as your view.
-The `Int` argument is how many results you would like to show.
--}
-update : UpdateConfig msg data -> Msg -> Int -> State -> List data -> ( State, Maybe msg )
-update (UpdateConfig config) (Msg msg) howManyToShow (State state) data =
-    let
-        ( newState, maybeMsg ) =
-            Internal.update config msg howManyToShow state data
-    in
-        ( State newState, maybeMsg )
-
-
-{-| Create the configuration for your `update` function (`UpdateConfig`).
-Say we have a `List Person` that we want to show as a series of options.
-We would create an `UpdateConfig` like so:
-
-    import Autocomplete
-    updateConfig : Autocomplete.UpdateConfig Msg Person
-    updateConfig =
-        Autocomplete.updateConfig
-            { toId = .name
-            , onKeyDown =
-                \code maybeId ->
-                    if code == 38 || code == 40 then
-                        Nothing
-                    else if code == 13 then
-                        Maybe.map SelectPerson maybeId
-                    else
-                        Just Reset
-            , onTooLow = Nothing
-            , onTooHigh = Nothing
-            , onMouseEnter = \_ -> Nothing
-            , onMouseLeave = \_ -> Nothing
-            , onMouseClick = \id -> Just <| SelectPerson id
-            , separateSelections = False
-            }
-
-You provide the following information in your autocomplete configuration:
-  - `toId` &mdash; turn a `Person` into a unique ID.
-  - `ul` &mdash; specify any non-behavioral attributes you'd like for the list menu.
-  - `li` &mdash; specify any non-behavioral attributes and children for a list item: both selection states are provided.
--}
 updateConfig :
     { toId : data -> String
     , onKeyDown : KeyCode -> Maybe String -> Maybe msg
     , onTooLow : Maybe msg
     , onTooHigh : Maybe msg
+    , onFocus : String -> Maybe msg
     , onMouseEnter : String -> Maybe msg
     , onMouseLeave : String -> Maybe msg
     , onMouseClick : String -> Maybe msg
     , separateSelections : Bool
     }
     -> UpdateConfig msg data
-updateConfig config =
-    UpdateConfig <| Internal.updateConfig config
+updateConfig { toId, onKeyDown, onTooLow, onTooHigh, onFocus, onMouseEnter, onMouseLeave, onMouseClick, separateSelections } =
+    { toId = toId
+    , onKeyDown = onKeyDown
+    , onTooLow = onTooLow
+    , onTooHigh = onTooHigh
+    , onFocus = onFocus
+    , onMouseEnter = onMouseEnter
+    , onMouseLeave = onMouseLeave
+    , onMouseClick = onMouseClick
+    , separateSelections = separateSelections
+    }
 
 
-{-| Add this to your `program`'s subscriptions so the the autocomplete menu will respond to keyboard input.
--}
-subscription : Sub Msg
-subscription =
-    Sub.map Msg Internal.subscription
+update : UpdateConfig msg data -> Msg -> Int -> State -> List data -> ( State, Maybe msg )
+update config msg howManyToShow state data =
+    case msg of
+        Focus id ->
+            (state, config.onFocus id)
+    
+        KeyDown keyCode ->
+            let
+                boundedList =
+                    List.map config.toId data
+                        |> List.take howManyToShow
+
+                newKey =
+                    navigateWithKey keyCode boundedList state.key
+            in
+                if newKey == state.key && keyCode == 38 then
+                    ( state
+                    , config.onTooHigh
+                    )
+                else if newKey == state.key && keyCode == 40 then
+                    ( state
+                    , config.onTooLow
+                    )
+                else if config.separateSelections then
+                    ( { state | key = newKey }
+                    , config.onKeyDown keyCode newKey
+                    )
+                else
+                    ( { key = newKey, mouse = newKey }
+                    , config.onKeyDown keyCode newKey
+                    )
+
+        MouseEnter id ->
+            ( resetMouseStateWithId config.separateSelections id state
+            , config.onMouseEnter id
+            )
+
+        MouseLeave id ->
+            ( resetMouseStateWithId config.separateSelections id state
+            , config.onMouseLeave id
+            )
+
+        MouseClick id ->
+            ( resetMouseStateWithId config.separateSelections id state
+            , config.onMouseClick id
+            )
+
+        NoOp ->
+            ( state, Nothing )
 
 
-{-|
-Take a list of `data` and turn it into an autocomplete menu.
-The `ViewConfig` argument is the configuration for the autocomplete view.
-`ViewConfig` describes the HTML we want to show for each item and the list.
-The `Int` argument is how many results you would like to show.
-The `State` argument describes what is selected via mouse and keyboard.
+resetMouseStateWithId : Bool -> String -> State -> State
+resetMouseStateWithId separateSelections id state =
+    if separateSelections then
+        { key = state.key, mouse = Just id }
+    else
+        { key = Just id, mouse = Just id }
 
-**Note:** The `State` and `List data` should live in your Model.
-The `ViewConfig` for the autocomplete belongs in your view code.
-`ViewConfig` should never exist in your model.
-Describe any potential autocomplete configurations statically.
-This pattern has been inspired by [Elm Sortable Table](http://package.elm-lang.org/packages/evancz/elm-sortable-table/latest).
--}
+
+getPreviousItemId : List String -> String -> String
+getPreviousItemId ids selectedId =
+    Maybe.withDefault selectedId <| List.foldr (getPrevious selectedId) Nothing ids
+
+
+getPrevious : String -> String -> Maybe String -> Maybe String
+getPrevious id selectedId resultId =
+    if selectedId == id then
+        Just id
+    else if (Maybe.withDefault "" resultId) == id then
+        Just selectedId
+    else
+        resultId
+
+
+getNextItemId : List String -> String -> String
+getNextItemId ids selectedId =
+    Maybe.withDefault selectedId <| List.foldl (getPrevious selectedId) Nothing ids
+
+
+navigateWithKey : Int -> List String -> Maybe String -> Maybe String
+navigateWithKey code ids maybeId =
+    case code of
+        38 ->
+            Maybe.map (getPreviousItemId ids) maybeId
+
+        40 ->
+            Maybe.map (getNextItemId ids) maybeId
+
+        _ ->
+            maybeId
+
+
+keyedDiv : List (Attribute Msg) -> List (String, Html Msg) -> Html Msg
+keyedDiv attributes children =
+    (Html.Keyed.node "div") attributes children
+
+
 view : ViewConfig data -> Int -> State -> List data -> Html Msg
-view (ViewConfig config) howManyToShow (State state) data =
-    Html.map Msg <| Internal.view config howManyToShow state data
+view config howManyToShow state data =
+    Html.div [ Html.Attributes.class "dropdown open" ]
+        [ viewList config howManyToShow state data ]
 
 
-{-| Presents an autocomplete menu with sections.
-You can follow the same instructions as described for `view`, providing a more advanced configuration and different data shape.
-`ViewWithSectionsConfig` sets up your autocomplete to handle sectioned data.
-The sectioned data becomes the new data argument for `viewWithSections`.
--}
-viewWithSections : ViewWithSectionsConfig data sectionData -> Int -> State -> List sectionData -> Html Msg
-viewWithSections (ViewWithSectionsConfig config) howManyToShow (State state) sections =
-    Html.map Msg <| Internal.viewWithSections config howManyToShow state sections
+viewItem : ViewConfig data -> State -> data -> Html Msg
+viewItem { toId, viewItemContent } { key, mouse } data =
+    let
+        id =
+            toId data
+
+        isSelected maybeId =
+            case maybeId of
+                Just someId ->
+                    someId == id
+
+                Nothing ->
+                    False
+    in
+        Html.button
+            [ Html.Attributes.classList
+                [ ( "dropdown-item", True )
+                , ( "bg-faded", (isSelected key) || (isSelected mouse) )
+                ]
+            , Html.Attributes.id id
+            , Html.Attributes.type' "button"
+            , Html.Events.onFocus (Focus id)
+            , Html.Events.onMouseEnter (MouseEnter id)
+            , Html.Events.onMouseLeave (MouseLeave id)
+            , Html.Events.onClick (MouseClick id)
+            ]
+            (List.map (Html.App.map (\html -> NoOp)) (viewItemContent  data))
 
 
-{-|
-HTML lists require `li` tags as children, so we allow you to specify everything about `li` HTML node except the nodeType.
--}
+viewList : ViewConfig data -> Int -> State -> List data -> Html Msg
+viewList config howManyToShow state data =
+    let
+        getKeyedItems datum =
+            ( config.toId datum, viewItem config state datum )
+    in
+        keyedDiv [ Html.Attributes.class "dropdown-menu" ]
+            (List.take howManyToShow data
+                |> List.map getKeyedItems
+            )
+
+
 type alias HtmlDetails msg =
     { attributes : List (Attribute msg)
     , children : List (Html msg)
     }
 
 
-{-|
-Configuration for your autocomplete, describing your menu and its items.
-
-**Note:** Your `ViewConfig` should never be held in your model. It should only appear in view code.
--}
-type ViewConfig data
-    = ViewConfig (Internal.ViewConfig data)
+type alias ViewConfig data =
+    { toId : data -> String
+    -- , ul : List (Attribute Never)
+    , viewItemContent : data -> List (Html Never)
+    }
 
 
-{-| Create the configuration for your `view` function (`ViewConfig`).
-Say we have a `List Person` that we want to show as a series of options.
-We would create a `ViewConfig` like so:
-
-    import Autocomplete
-    viewConfig : Autocomplete.Config Person
-    viewConfig =
-      let
-          customizedLi keySelected mouseSelected person =
-              { attributes =
-                  [ classList [ ( "autocomplete-item", True )
-                              , ( "key-selected", keySelected )
-                              , ( "mouse-selected", mouseSelected )
-                              ]
-                  ]
-              , children = [ Html.text person.name ]
-              }
-      in
-          Autocomplete.viewConfig
-              { toId = .name
-              , ul = [ class "autocomplete-list" ]
-              , li = customizedLi
-              }
-You provide the following information in your autocomplete configuration:
-  - `toId` &mdash; turn a `Person` into a unique ID. This lets us use
-  [`Html.Keyed`][keyed] under the hood to make sorting faster.
-  - `ul` &mdash; specify any non-behavioral attributes you'd like for the list menu.
-  - `li` &mdash; specify any non-behavioral attributes and children for a list item: both selection states are provided.
-See the [examples][] to get a better understanding!
-[keyed]: http://package.elm-lang.org/packages/elm-lang/html/latest/Html-Keyed
-[examples]: https://github.com/thebritican/elm-autocomplete/tree/master/examples
--}
 viewConfig :
     { toId : data -> String
-    , ul : List (Attribute Never)
-    , li : KeySelected -> MouseSelected -> data -> HtmlDetails Never
+    -- , ul : List (Attribute Never)
+    , viewItemContent : data -> List (Html Never)
     }
     -> ViewConfig data
-viewConfig config =
-    ViewConfig <| Internal.viewConfig config
-
-
-{-|
-Configuration for your autocomplete, describing your menu, its sections, and its items.
-
-**Note:** This should never live in your model.
--}
-type ViewWithSectionsConfig data sectionData
-    = ViewWithSectionsConfig (Internal.ViewWithSectionsConfig data sectionData)
-
-
-{-| The same configuration as viewConfig, but provide a section configuration as well.
--}
-viewWithSectionsConfig :
-    { toId : data -> String
-    , ul : List (Attribute Never)
-    , li : KeySelected -> MouseSelected -> data -> HtmlDetails Never
-    , section : SectionConfig data sectionData
+-- viewConfig { toId, ul, viewItemContent } =
+viewConfig { toId, viewItemContent } =
+    { toId = toId
+    -- , ul = ul
+    , viewItemContent = viewItemContent
     }
-    -> ViewWithSectionsConfig data sectionData
-viewWithSectionsConfig config =
-    ViewWithSectionsConfig
-        <| case config.section of
-            SectionConfig section ->
-                Internal.viewWithSectionsConfig { config | section = section }
-
-
-{-| The configuration for a section of the menu.
-
-**Note:** This should never live in your model.
--}
-type SectionConfig data sectionData
-    = SectionConfig (Internal.SectionConfig data sectionData)
-
-
-{-| Describe everything about a Section HTML node.
--}
-type alias SectionNode msg =
-    { nodeType : String
-    , attributes : List (Attribute msg)
-    , children : List (Html msg)
-    }
-
-
-{-| Create the `SectionConfig` for your `view` function.
-Say we have a `List Century` that we want to show as a series of sections.
-We would create a `SectionConfig` like so:
-
-    type alias Century =
-      { title : String
-      , people : List Person
-      }
-
-    import Autocomplete
-    sectionConfig : Autocomplete.SectionConfig Person Century
-    sectionConfig =
-        Autocomplete.sectionConfig
-            { toId = .title
-            , getData = .people
-            , ul = [ class "autocomplete-section-list" ]
-            , li =
-                \section ->
-                    { nodeType = "div"
-                    , attributes = [ class "autocomplete-section-item" ]
-                    , children =
-                        [ div [ class "autocomplete-section-box" ]
-                            [ strong [ class "autocomplete-section-text" ] [ text section.title ]
-                            ]
-                        ]
-                    }
-            }
-
-
-
-You provide the following information in your autocomplete configuration:
-  - `toId` &mdash; turn a `Century` into a unique ID.
-  - `getData` &mdash; extract the data from `Century`, in this case: `List Person`.
-  - `ul` &mdash; specify any non-behavioral attributes you'd like for the section list.
-  - `li` &mdash; specify any non-behavioral attributes and children for a section.
--}
-sectionConfig :
-    { toId : sectionData -> String
-    , getData : sectionData -> List data
-    , ul : List (Attribute Never)
-    , li : sectionData -> SectionNode Never
-    }
-    -> SectionConfig data sectionData
-sectionConfig section =
-    SectionConfig <| Internal.sectionConfig section
--- src/Native/Tricks.js
+++
@@ -1,7 +0,0 @@
-var _retruco$retruco_ui$Native_Tricks = function() {
-
-return {
-    trickyMap: function(x) { return x; }
-};
-
-}();
