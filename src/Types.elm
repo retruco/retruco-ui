c5c3ac2fd80683ba27f6286529df14c516fb535f
Emmanuel Raviart <emmanuel@raviart.com>
2017-08-13 17:56:20 +0200

    Remove obsolete code.

--- src/Types.elm
+++ src/Types.elm
@@ -14,27 +14,6 @@ type alias Argument =
     }
 
 
-
--- type alias Abuse =
---     { statementId : String
---     }
--- type alias Argument =
---     { argumentType : ArgumentType
---     , claimId : String
---     , groundId : String
---     }
--- type ArgumentType
---     = Because
---     | But
---     | Comment
---     | Example
--- type AutocompleteMenuState
---     = AutocompleteMenuHidden
---     | AutocompleteMenuSleeping
---     | AutocompleteMenuLoading
---     | AutocompleteMenuVisible
-
-
 type alias Ballot =
     { deleted : Bool
     , id : String
@@ -77,14 +56,6 @@ type alias CardAutocompletion =
 type alias CardsAutocompletionBody =
     { data : List CardAutocompletion
     }
-
-
-
--- type alias Citation =
---     { citedId : String
---     , eventId : String
---     , personId : String
---     }
 
 
 type alias Collection =
@@ -150,14 +121,6 @@ type alias DocumentMetadata =
     }
 
 
-
--- type alias Event =
---     { name : String
---     }
--- type alias EventForm =
---     Event
-
-
 type Field
     = BooleanField Bool
     | CardIdField String
@@ -180,27 +143,6 @@ type alias Flags =
 
 type alias FormErrors =
     Dict String String
-
-
-
--- type alias ModelFragment a =
---     { a
---         | ballotById : Dict String Ballot
---         , statementById : Dict String Statement
---         , statementIds : List String
---     }
--- type alias Person =
---     { name : String
---     , twitterName : String
---     }
--- type alias PersonForm =
---     Person
--- type alias Plain =
---     { languageCode : String
---     , name : String
---     }
--- type alias PlainForm =
---     Plain
 
 
 type alias PopularTag =
@@ -241,65 +183,6 @@ type alias SearchCriteria =
     , sort : String
     , termMaybe : Maybe String
     }
-
-
-
--- type alias Statement =
---     { ballotIdMaybe : Maybe String
---     , createdAt : String
---     , custom : StatementCustom
---     , deleted : Bool
---     , groundIds : List String
---     , id : String
---     , isAbuse : Bool
---     , ratingCount : Int
---     , ratingSum : Int
---     }
--- type alias StatementAutocompletion =
---     { autocomplete : String
---     , distance : Float
---     , statement : Statement
---     }
--- type StatementCustom
---     = AbuseCustom Abuse
---     | ArgumentCustom Argument
---     | CitationCustom Citation
---     | EventCustom Event
---     | PersonCustom Person
---     | PlainCustom Plain
---     | TagCustom Tag
--- type alias StatementEmbed =
---     { autocomplete : String
---     , autocompleteMenuState : AutocompleteMenuState
---     , autocompleter : Autocomplete.State
---     , autocompletions : List StatementAutocompletion
---     , editedMaybe : Maybe PersonForm
---     , selectedMaybe : Maybe StatementAutocompletion
---     }
--- type alias StatementForm =
---     { argumentType : String
---     , cited : StatementEmbed
---     , citedId : String
---     , claimId : String
---     , errors : FormErrors
---     , event : StatementEmbed
---     , eventId : String
---     , groundId : String
---     , kind : String
---     , languageCode : String
---     , name : String
---     , person : StatementEmbed
---     , personId : String
---     , statementId : String
---     , twitterName : String
---     }
--- type alias StatementsAutocompletionBody =
---     { data : List StatementAutocompletion
---     }
--- type alias Tag =
---     { name : String
---     , statementId : String
---     }
 
 
 type alias TypedValue =
@@ -366,102 +249,6 @@ cardSubTypeIdsIntersect : List String -> List String -> Bool
 cardSubTypeIdsIntersect cardSubTypeIds1 cardSubTypeIds2 =
     List.any (\subTypeId -> List.member subTypeId cardSubTypeIds2)
         cardSubTypeIds1
-
-
-
--- convertArgumentTypeToString : ArgumentType -> String
--- convertArgumentTypeToString argumentType =
---     case argumentType of
---         Because ->
---             "because"
---         But ->
---             "but"
---         Comment ->
---             "comment"
---         Example ->
---             "example"
--- convertStatementCustomToKind : StatementCustom -> String
--- convertStatementCustomToKind statementCustom =
---     case statementCustom of
---         AbuseCustom abuse ->
---             "Abuse"
---         ArgumentCustom argument ->
---             "Argument"
---         CitationCustom plain ->
---             "Citation"
---         EventCustom plain ->
---             "Event"
---         PersonCustom plain ->
---             "Person"
---         PlainCustom plain ->
---             "PlainStatement"
---         TagCustom tag ->
---             "Tag"
--- convertStatementFormToCustom : StatementForm -> StatementCustom
--- convertStatementFormToCustom form =
---     case form.kind of
---         "Abuse" ->
---             AbuseCustom
---                 { statementId = form.statementId
---                 }
---         "Argument" ->
---             ArgumentCustom
---                 { argumentType =
---                     case form.argumentType of
---                         "because" ->
---                             Because
---                         "but" ->
---                             But
---                         "comment" ->
---                             Comment
---                         "example" ->
---                             Example
---                         _ ->
---                             Comment
---                 , claimId = form.claimId
---                 , groundId = form.groundId
---                 }
---         "Citation" ->
---             CitationCustom
---                 { citedId = form.citedId
---                 , eventId = form.eventId
---                 , personId = form.personId
---                 }
---         "Event" ->
---             EventCustom
---                 { name = form.name
---                 }
---         "Person" ->
---             PersonCustom
---                 { name = form.name
---                 , twitterName = form.twitterName
---                 }
---         "PlainStatement" ->
---             PlainCustom
---                 { languageCode = form.languageCode
---                 , name = form.name
---                 }
---         "Tag" ->
---             TagCustom
---                 { name = form.name
---                 , statementId = form.statementId
---                 }
---         _ ->
---             -- TODO: Return a Result instead of a dummy PlainCustom.
---             PlainCustom
---                 { languageCode = "en"
---                 , name = "Unknown kind: " ++ form.kind
---                 }
--- filterPrefix : String -> Dict String value -> Dict String value
--- filterPrefix prefix dict =
---     let
---         prefixLength =
---             String.length prefix
---     in
---         Dict.filter (\key value -> String.startsWith prefix key) dict
---             |> Dict.toList
---             |> List.map (\( key, value ) -> ( String.dropLeft prefixLength key, value ))
---             |> Dict.fromList
 
 
 getCard : Dict String Card -> String -> Card
@@ -537,50 +324,6 @@ initDataIds =
     , users = Dict.empty
     , values = Dict.empty
     }
-
-
-
--- initEventForm : EventForm
--- initEventForm =
---     { name = ""
---     }
--- initPersonForm : PersonForm
--- initPersonForm =
---     { name = ""
---     , twitterName = ""
---     }
--- initPlainForm : PlainForm
--- initPlainForm =
---     { languageCode = "en"
---     , name = ""
---     }
--- initStatementEmbed : StatementEmbed
--- initStatementEmbed =
---     { autocomplete = ""
---     , autocompleteMenuState = AutocompleteMenuHidden
---     , autocompleter = Autocomplete.empty
---     , autocompletions = []
---     , editedMaybe = Nothing
---     , selectedMaybe = Nothing
---     }
--- initStatementForm : StatementForm
--- initStatementForm =
---     { argumentType = ""
---     , cited = initStatementEmbed
---     , citedId = ""
---     , claimId = ""
---     , errors = Dict.empty
---     , event = initStatementEmbed
---     , eventId = ""
---     , groundId = ""
---     , kind = "PlainStatement"
---     , languageCode = "en"
---     , name = ""
---     , person = initStatementEmbed
---     , personId = ""
---     , statementId = ""
---     , twitterName = ""
---     }
 
 
 mergeData : DataProxy a -> DataProxy b -> DataProxy b
